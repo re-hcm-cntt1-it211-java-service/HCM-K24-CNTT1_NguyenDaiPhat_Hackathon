c4f9604e5b07847412800951d99c43afa37444d2
PhatNguyen06 <nguyendaiphat06@gmail.com>
2026-06-16 09:28:34 +0700

    update

--- HCM-K24-CNTT1-NguyenDaiPhat-002/src/main/java/com/example/hcmk24cntt1nguyendaiphat002/aspect/LoggerMethod.java
+++ HCM-K24-CNTT1-NguyenDaiPhat-002/src/main/java/com/example/hcmk24cntt1nguyendaiphat002/aspect/LoggerMethod.java
@@ -1,16 +1,18 @@
 package com.example.hcmk24cntt1nguyendaiphat002.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
+@Slf4j
 @Aspect
 public class LoggerMethod {
 
-//    @Before("execution(*com.example.hcmk24cntt1nguyendaiphat002..*(..))")
-//    public Object loggerMethod(JoinPoint joinPoint) {
-//        String nameMethod = joinPoint.getSignature().getName();
-//
-//        joinPoint
-//    }
+    @Before("execution(*com.example.hcmk24cntt1nguyendaiphat002..*(..))")
+    public void loggerMethod(JoinPoint joinPoint) {
+        String nameMethod = joinPoint.getSignature().getName();
+
+        log.info("methou " + nameMethod);
+    }
 }
